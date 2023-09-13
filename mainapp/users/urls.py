23bf78e904f3ca050c1201eb27da963e3fd4abcb
Fedyanin <fedyanin.v.v@yandex.ru>
2023-09-13 16:53:06 +0300

    Minor Changes 13/09/23

--- mainapp/users/urls.py
+++ mainapp/users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from users.views import UsersView, UserDetail
 
 urlpatterns = [
-    path('users', UsersView.as_view()),
+    path('users/', UsersView.as_view()),
     path("users/<int:id>", UserDetail.as_view()),
 ]
